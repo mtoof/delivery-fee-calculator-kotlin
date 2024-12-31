f4a9c1c7e0271086c9b4054b0ebdc9cb42956a9c
--get <mojtaba.toof@gmail.com>
2024-12-31 18:55:09 +0200

    removed @SpringBootTest from DeliveryFeeApplicationServiceTest and added extra tests

--- src/test/kotlin/example/deliveryfee/DeliveryFeeApplicationServiceTest.kt
+++ src/test/kotlin/example/deliveryfee/DeliveryFeeApplicationServiceTest.kt
@@ -1,62 +1,77 @@
 package example.deliveryfee
 
 import example.deliveryfee.service.DeliveryFeeService
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import java.time.ZonedDateTime
 import kotlin.test.assertEquals
 
 
-@SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
 class DeliveryFeeApplicationServiceTest() {
-
-    @Autowired
     lateinit var deliveryFeeService: DeliveryFeeService
+
+    @BeforeEach
+    fun setUp(){
+        deliveryFeeService = DeliveryFeeService()
+    }
 
     @Test
     fun `test small order surcharge`(){
         val surcharge = deliveryFeeService.calculateCartValueSurcharge(790)
-
-        assertEquals(210, surcharge)
+        assertEquals(210, surcharge) //10e-7.9 = 2.10e
     }
 
     @Test
     fun `test delivery distance surcharge`(){
         val surcharge: Int = deliveryFeeService.calculateDeliveryDistanceFee(2150)
-        //2e for first 1000m + 1e x for every extra less than equal 500m -> 2 + (1 x 3) = 5 euro
-        assertEquals(500, surcharge)
+        assertEquals(500, surcharge)//2e for first 1000m + 1e x for every extra less than equal 500m -> 2 + (1 x 3) = 5 euro
     }
 
     @Test
     fun `test zero for four items`() {
         val surcharge: Int = deliveryFeeService.calculateNumberOfItemsFee(4)
-        //Example 1: If the number of items is 4, no extra surcharge
-        assertEquals(0, surcharge)
+        assertEquals(0, surcharge) //If the number of items is 4, no extra surcharge
     }
 
     @Test
     fun `test for ten items`() {
         val surcharge: Int = deliveryFeeService.calculateNumberOfItemsFee(10)
-        //Example 3: If the number of items is 10, 3€ surcharge (6 x 50 cents) is added
-        assertEquals(300, surcharge)
+        assertEquals(300, surcharge) //If the number of items is 10, 3€ surcharge (6 x 50 cents) is added
     }
 
     @Test
-    fun `test for bulk fee surcharge for thirteen items`() {
+    fun `test for bulk fee surcharge for more than 12 items`() {
         val surcharge: Int = deliveryFeeService.calculateNumberOfItemsFee(13)
-        //Example 4: If the number of items is 13, 5,70€ surcharge is added ((9 * 50 cents) + 1,20€)
-        assertEquals(570, surcharge)
+        assertEquals(570, surcharge) //If the number of items is 13, 5,70€ surcharge is added ((9 * 50 cents) + 1,20€)
     }
 
     @Test
-    fun `test RushHour`(){
+    fun `test isRushHour`(){
         val surcharge: Int = deliveryFeeService.isRushHour(
             ZonedDateTime.parse("2024-12-06T15:10:00Z")
             , 500)
-        // deliveryFee * 1.2x
-        assertEquals(600, surcharge)
+        assertEquals(600, surcharge) // deliveryFee * 1.2x
+    }
+
+    @Test
+    fun `test for 200 euro cart_value`() {
+        val deliveryFee: Int = deliveryFeeService.calculateDeliveryFee(
+            20000,
+            10000,
+            20,
+            timeStamp = ZonedDateTime.parse("2024-12-06T15:10:00Z")
+        )
+        assertEquals(0, deliveryFee)
+    }
+
+    @Test
+    fun `test for max delivery fee`() {
+        val deliveryFee: Int = deliveryFeeService.calculateDeliveryFee(
+            790,
+            4100,
+            20,
+            timeStamp = ZonedDateTime.parse("2024-12-06T15:10:00Z")
+        )
+        assertEquals(1500, deliveryFee)// 210+900+920 = (2030 * 1.2x) > 1500
     }
 }
