e15939a9f98d4148975362f44a54c3355d047533
mtoof <mojtaba.toof@gmail.com>
2025-01-12 13:55:06 +0200

    added an exception handler for incorrect data type to show where the error happened

--- src/main/kotlin/example/deliveryfee/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/example/deliveryfee/exception/GlobalExceptionHandler.kt
@@ -1,6 +1,10 @@
 package example.deliveryfee.exception
 
+import com.fasterxml.jackson.databind.JsonMappingException
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -8,9 +12,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException::class)
-    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<String> {
-        return ResponseEntity.badRequest().body(e.message)
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(err: HttpMessageNotReadableException): ResponseEntity<Map<String, String>> {
+        val errorDetails = mutableMapOf<String, String>()
+
+        errorDetails["error"] = "Invalid input data"
+
+        val cause = err.cause
+        if (cause is JsonMappingException){
+            val fieldPath = cause.path?.joinToString("."){it.fieldName ?: "[Unknown]"}
+
+            errorDetails["field"] = fieldPath ?: "Unknown field"
+            errorDetails["message"] = cause.originalMessage ?: "Invalid data"
+        }
+        else{
+            errorDetails["message"] = err.message ?: "Error processing input"
+        }
+        return ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
